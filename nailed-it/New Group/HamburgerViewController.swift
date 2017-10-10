4d351b3a10fb51a9b55f922c03604a22f0106fa8
Lia Zadoyan <liazadoyan@gmail.com>
2017-10-10 14:59:17 -0700

    initial view controllers for hamburger menu

---
+++ nailed-it/New Group/HamburgerViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  nailed-it
+//
+//  Created by Lia Zadoyan on 10/10/17.
+//  Copyright © 2017 Lia Zadoyan. All rights reserved.
+//
+
+import UIKit
+
+class HamburgerViewController: UIViewController {
+
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var leftMarginConstraint: NSLayoutConstraint!
+    
+    var originalLeftMargin: CGFloat = 0
+    
+    var menuViewController: UIViewController! {
+        didSet(oldMenuViewController) {
+            view.layoutIfNeeded()
+            
+            if oldMenuViewController != nil {
+                oldMenuViewController.willMove(toParentViewController: nil)
+                oldMenuViewController.view.removeFromSuperview()
+                oldMenuViewController.didMove(toParentViewController: nil)
+            }
+            menuViewController.willMove(toParentViewController: self)
+            menuView.addSubview(menuViewController.view)
+            menuViewController.didMove(toParentViewController: self)
+        }
+    }
+    
+    var contentViewController: UIViewController! {
+        didSet(oldContentViewController) {
+            view.layoutIfNeeded()
+            
+            if oldContentViewController != nil {
+                oldContentViewController.willMove(toParentViewController: nil)
+                oldContentViewController.view.removeFromSuperview()
+                oldContentViewController.didMove(toParentViewController: nil)
+            }
+            contentViewController.willMove(toParentViewController: self)
+            contentView.addSubview(contentViewController.view)
+            contentViewController.didMove(toParentViewController: self)
+            UIView.animate(withDuration: 0.3, animations: {
+                self.leftMarginConstraint.constant = 0
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let menuViewController = storyboard.instantiateViewController(withIdentifier: "MenuViewController") as! MenuViewController
+        let hamburgerController = self
+        menuViewController.hamburgerViewController = hamburgerController
+        self.menuViewController = menuViewController
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    @IBAction func onPanGesture(_ sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translation(in: view)
+        
+        let velocity = sender.velocity(in: view)
+        
+        if sender.state == UIGestureRecognizerState.began {
+            originalLeftMargin = leftMarginConstraint.constant
+        } else if sender.state == UIGestureRecognizerState.changed {
+            leftMarginConstraint.constant = originalLeftMargin + translation.x
+        } else if sender.state == UIGestureRecognizerState.ended {
+            UIView.animate(withDuration: 0.3, animations: {
+                if velocity.x > 0 {
+                    self.leftMarginConstraint.constant = self.view.frame.size.width - 100
+                } else {
+                    self.leftMarginConstraint.constant = 0
+                }
+                self.view.layoutIfNeeded()
+            })
+            
+            
+        }
+    }
+
+
+}
+
---
+++ nailed-it/New Group/MenuViewController.swift
@@ -0,0 +1,75 @@
+//
+//  MenuViewController.swift
+//  nailed-it
+//
+//  Created by Lia Zadoyan on 10/10/17.
+//  Copyright © 2017 Lia Zadoyan. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    private var profileViewController: UIViewController!
+    private var mentionsViewController: UIViewController!
+    private var timelineViewController: UIViewController!
+    private var accountsViewController: UIViewController!
+    
+    var viewControllers: [UIViewController] = []
+    
+    var hamburgerViewController: HamburgerViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+//        profileViewController = storyboard.instantiateViewController(withIdentifier: "ProfileNavigationController")
+//        mentionsViewController = storyboard.instantiateViewController(withIdentifier: "MentionsNavigationController")
+//        timelineViewController = storyboard.instantiateViewController(withIdentifier: "TweetsNavigationController")
+//        accountsViewController = storyboard.instantiateViewController(withIdentifier: "AccountsNavigationController")
+//
+//        viewControllers.append(profileViewController)
+//        viewControllers.append(mentionsViewController)
+//        viewControllers.append(timelineViewController)
+//        viewControllers.append(accountsViewController)
+//
+//        hamburgerViewController.contentViewController = viewControllers[2]
+    
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    @IBAction func onProfileSelected(_ sender: Any) {
+        hamburgerViewController.contentViewController = viewControllers[0]
+    }
+    
+    
+    @IBAction func onTimelineSelected(_ sender: Any) {
+        hamburgerViewController.contentViewController = viewControllers[2]
+        
+    }
+    
+    @IBAction func onMentionsSelected(_ sender: Any) {
+        hamburgerViewController.contentViewController = viewControllers[1]
+        
+    }
+    
+    
+    @IBAction func onAccountsSelected(_ sender: Any) {
+        hamburgerViewController.contentViewController = viewControllers[3]
+    }
+}
