d15c6a77a9e55e539b41dc803051de7ca3d055cd
Terra Oldham <toldham@hearsaycorp.com>
2017-10-16 20:32:51 -0700

    Updating Polish Library UI + Brand

--- nailed-it/Views/PolishCollectionViewCell.swift
+++ nailed-it/Views/PolishCollectionViewCell.swift
@@ -12,13 +12,25 @@ class PolishCollectionViewCell: UICollectionViewCell {
     
     var polishColor: PolishColor? {
         didSet {
+            colorView.layer.cornerRadius = colorView.frame.size.height / 2
+            colorView.clipsToBounds = true
             colorView.backgroundColor = polishColor?.getUIColor()
+            whiteView.layer.cornerRadius = whiteView.frame.size.height / 2
+            whiteView.backgroundColor = UIColor.white
+            colorView.layer.borderWidth = 1
+            colorView.layer.borderColor = UIColor.gray.cgColor
+            whiteView.layer.borderWidth = 1
+            whiteView.layer.borderColor = UIColor.gray.cgColor
+            
             colorName.text = polishColor?.displayName ?? ""
+            colorBrand.text = polishColor?.brand ?? ""
         }
     }
     
     @IBOutlet weak var colorView: UIView!
     @IBOutlet weak var colorName: UILabel!
+    @IBOutlet weak var colorBrand: UILabel!
+    @IBOutlet weak var whiteView: UIView!
     
     
     
