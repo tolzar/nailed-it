ed667635dea8d1f85292080b89a1189db7a6f1a2
Terra Oldham <toldham@hearsaycorp.com>
2017-10-25 18:42:33 -0700

    Adding shadows, updating font for polish libraries

--- nailed-it/Views/PolishCollectionViewCell.swift
+++ nailed-it/Views/PolishCollectionViewCell.swift
@@ -14,10 +14,6 @@ class PolishCollectionViewCell: UICollectionViewCell {
         didSet {
             colorView.backgroundColor = polishColor?.getUIColor()
             whiteView.layer.cornerRadius = whiteView.frame.size.height / 2
-            whiteView.clipsToBounds = true
-            
-            colorView.layer.cornerRadius = 20
-            colorView.clipsToBounds = true
 
             whiteView.backgroundColor = UIColor.white
             colorView.layer.borderWidth = 1
@@ -42,5 +38,44 @@ class PolishCollectionViewCell: UICollectionViewCell {
     }
     
     
+}
+
+extension UIView {
+    
+    @IBInspectable var shadow: Bool {
+        get {
+            return layer.shadowOpacity > 0.0
+        }
+        set {
+            if newValue == true {
+                self.addShadow()
+            }
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        set {
+            self.layer.cornerRadius = newValue
+            
+            // Don't touch the masksToBound property if a shadow is needed in addition to the cornerRadius
+            if shadow == false {
+                self.layer.masksToBounds = true
+            }
+        }
+    }
+    
+    
+    func addShadow(shadowColor: CGColor = UIColor.black.cgColor,
+                   shadowOffset: CGSize = CGSize(width: 1.0, height: 2.0),
+                   shadowOpacity: Float = 0.4,
+                   shadowRadius: CGFloat = 3.0) {
+        layer.shadowColor = shadowColor
+        layer.shadowOffset = shadowOffset
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowRadius = shadowRadius
+    }
 }
 
